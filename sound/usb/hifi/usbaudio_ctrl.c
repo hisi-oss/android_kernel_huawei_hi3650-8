ec174b39adccce76c63660ae957f4ad287f1deef
R0rt1z2 <me@r0rt1z2.com>
2023-06-17 17:07:46 +0000

    sound: hifi: Modify `shortname` empty check logic
    - error: address of array 'chip->card->shortname' will always evaluate to 'true' [-Werror,-Wpointer-bool-conversion]

--- sound/usb/hifi/usbaudio_ctrl.c
+++ sound/usb/hifi/usbaudio_ctrl.c
@@ -283,7 +283,7 @@ void usbaudio_ctrl_set_chip(struct snd_usb_audio *chip)
 	mutex_lock(&connect_mutex);
 	if (usbaudio_hifi && chip) {
 		usbaudio_hifi->wake_up = true;
-		if (chip->card && chip->dev->serial && chip->card->shortname && (sizeof(chip->card->shortname) <= 256)){
+                if (chip->card && chip->dev->serial && chip->card->shortname[0] != '\0' && (sizeof(chip->card->shortname) <= 256)) {
 			#ifdef CONFIG_HUAWEI_DSM
 			if (hisi_usb_using_hifi_usb(chip->dev))
 				audio_dsm_report_info(AUDIO_CODEC, DSM_USBAUDIO_INFO, "shortname:%s usbid %x serial %s usbphy %s \n", chip->card->shortname, chip->usb_id, chip->dev->serial, "hifi");
